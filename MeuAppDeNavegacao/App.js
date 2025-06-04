6f396ca9c89e6272b32a1d3b0374effe9fbeb72a
maju2107 <maju.mariajuliaaraujo@gmail.com>
2025-06-04 12:08:52 -0300

    login quase

--- MeuAppDeNavegacao/App.js
+++ MeuAppDeNavegacao/App.js
@@ -5,6 +5,7 @@ import HomeScreen from './src/screens/HomeScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 import LoginScreen  from './src/screens/LoginScreen';
+import CadastroScreen from './src/screens/CadastroScreen';
 
 
 const Stack = createStackNavigator();
@@ -12,11 +13,12 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Cadastro">
         <Stack.Screen name="Home" options={{headerShown:false}} component={HomeScreen}/> 
         <Stack.Screen name="Details" component={DetailsScreen}/>
         <Stack.Screen name="Profile" component={ProfileScreen}/>
         <Stack.Screen name="Login" options={{headerShown:false}} component={LoginScreen}/> 
+        <Stack.Screen name="Cadastro" options={{headerShown:false}} component={CadastroScreen}/> 
       </Stack.Navigator>
     </NavigationContainer>
   );
--- MeuAppDeNavegacao/src/screens/CadastroScreen.js
+++ MeuAppDeNavegacao/src/screens/CadastroScreen.js
@@ -1,20 +1,19 @@
-// Criar a tela de Login e deixar ela como 1ª tela do App.js;
-//Objetos da tela de login: 2 campos (login/senha) e botão "Entrar";
-//Criar navegação entre as telas: Home, Details e Profile;
-//Na tela de Home retirar a opção "Goback" (final do pdf);
-//Verificar se o usuário digitou o login e a senha corretamente;
-
 import React from 'react';
 import { View, Text, Button, StyleSheet, Dimensions, TextInput, ScrollView } from 'react-native';
 const windowWidth = Dimensions.get('window').width;
 const windowHeigth = Dimensions.get('window').height;
 
-export default function LoginScreen({ navigation }) {
+export default function CadastroScreen({ navigation }) {
     
     return (
         <ScrollView style={styles.all}>
             <View style={styles.container}>
-                <Text style={styles.title}>Login</Text>
+                <Text style={styles.title}>Cadastre-se!</Text>
+
+                <TextInput style={styles.input}
+                    placeholder='Nome de usuário:'
+        
+                />
                 <TextInput style={styles.input}
                     placeholder='e-mail:'
                 />
@@ -24,9 +23,9 @@ export default function LoginScreen({ navigation }) {
                 />
                 <View style={styles.buttonContainer}>
                     <Button
-                        title="Logar"
+                        title="Cadastrar"
                         testID='true'
-                        onPress={() => navigation.navigate('Home')}
+                        onPress={() => navigation.navigate('Login')}
                     />
                 </View>
             </View>
@@ -52,7 +51,7 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
     buttonContainer: {
-        backgroundColor: '#add8e6', // Cor de fundo do container do botão
+        backgroundColor: '#f0f8ff', // Cor de fundo do container do botão
         margin: 10,
         width: windowWidth * 0.5, // 50% da largura da tela
         borderRadius: 5,
